365238d7bbeca195ca3b85ad1c0b48380c32a14a
322332 <gokhan.demirtas@msn.com>
2023-04-01 11:57:31 +0300

    Add react-router-dom, add mui template

--- src/components/Component3.js
+++ src/components/Component3.js
@@ -1,8 +1,29 @@
 import React from "react";
-import { useSelector } from "react-redux";
 
 export default function Component3(props) {
-  
-  console.log("comp3")
-  return <div>""</div>;
+  // https://react.dev/learn
+  const arr = ["gökhan", "veysel", "sinan"];
+  const [loading, setLoading] = React.useState(false);
+
+  function showItem() {
+    setTimeout(() => {
+      setLoading(true);
+    }, 2000);
+  }
+
+  function hideItem() {
+    setLoading(false);
+  }
+
+  return (
+    <div>
+      {loading ? <div className="asd">"gösteriliyor"</div> : null}
+
+      <button onClick={showItem}>göster</button>
+      <button onClick={hideItem}>sil</button>
+      {arr.map((item) => (
+        <div key={item}>{item}</div>
+      ))}
+    </div>
+  );
 }

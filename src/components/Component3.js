00bc0789be0e4eeabd411c9c5a1be982f7d5a4ae
322332 <gokhan.demirtas@msn.com>
2023-04-15 11:14:26 +0300

    Add api

--- src/components/Component3.js
+++ src/components/Component3.js
@@ -1,29 +1,119 @@
 import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { getUsers, addUser } from "../api/users/users";
+import {
+  Button,
+  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  DialogContentText,
+  DialogActions,
+  TextField,
+} from "@mui/material";
 
-export default function Component3(props) {
-  // https://react.dev/learn
-  const arr = ["gökhan", "veysel", "sinan"];
-  const [loading, setLoading] = React.useState(false);
+function UserModal({ handleClose }) {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-  function showItem() {
-    setTimeout(() => {
-      setLoading(true);
-    }, 2000);
-  }
-
-  function hideItem() {
-    setLoading(false);
-  }
+  const handleaddUser = () => {
+    addUser({ username, password }).then((response) => {
+      alert(response.data.message);
+      handleClose();
+    });
+  };
 
   return (
-    <div>
-      {loading ? <div className="asd">"gösteriliyor"</div> : null}
+    <Dialog open onClose={handleClose}>
+      <DialogTitle>Subscribe</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          onChange={(e) => setUsername(e.target.value)}
+          label="Username"
+          type="text"
+          fullWidth
+          variant="standard"
+        />
+        <TextField
+          autoFocus
+          onChange={(e) => setPassword(e.target.value)}
+          margin="dense"
+          id="password"
+          label="Password"
+          type="password"
+          fullWidth
+          variant="standard"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={handleaddUser}>Save</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
 
-      <button onClick={showItem}>göster</button>
-      <button onClick={hideItem}>sil</button>
-      {arr.map((item) => (
-        <div key={item}>{item}</div>
-      ))}
-    </div>
+export default function Component3(props) {
+  const [loading, setLoading] = React.useState(false);
+  const [modal, setModal] = React.useState("close");
+
+  // datagrid
+  const [rows, setRows] = React.useState([]);
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "userId",
+      headerName: "First name",
+      flex: 1,
+    },
+    {
+      field: "title",
+      headerName: "First name",
+      flex: 1,
+    },
+    {
+      field: "completed",
+      headerName: "First name",
+      flex: 1,
+    },
+  ];
+
+  React.useEffect(() => {
+    const handleGetUsers = () => {
+      getUsers()
+        .then((response) => {
+          setRows(response.data);
+        })
+        .catch((error) => {
+          alert(error.response.data.message);
+        });
+    };
+
+    handleGetUsers();
+  }, []);
+
+  const handleClose = () => {
+    setModal("close");
+  };
+
+  return (
+    <Box sx={{ width: "100%", height: "60vh" }}>
+      <Box sx={{ width: "100%", height: 60 }}>
+        <Button onClick={() => setModal("add")}>Ekle</Button>
+      </Box>
+      <DataGrid
+        rows={rows}
+        loading={loading}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+      />
+      {modal != "close" ? <UserModal handleClose={handleClose} /> : null}
+    </Box>
   );
 }
